12d69d8b062061b9cae8e052665fb039e6b89527
Fair-de-Ragon <fairderagon@yandex.ru>
2022-12-12 23:10:43 +0300

    My first working test

--- app/src/main/java/com/fdrinc/qaapp/AccessibilityService/NodeInfo.kt
+++ app/src/main/java/com/fdrinc/qaapp/AccessibilityService/NodeInfo.kt
@@ -1,26 +1,60 @@
 package com.fdrinc.qaapp.AccessibilityService
 
+import android.os.Bundle
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
+import androidx.core.os.bundleOf
+import com.fdrinc.qaapp.MainActivity
+import kotlinx.coroutines.delay
 
 class NodeInfo : AccessibilityNodeInfo() {
-    fun firstTest(root: AccessibilityNodeInfo) {
+
+
+    suspend fun firstTest() {
+
+        var isDone = false
+
+        val nameText = Bundle()
+        val whereToBuyText = Bundle()
+        val descriptionText = Bundle()
+        nameText.putString(AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE, "Сыр")
+        whereToBuyText.putString(AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE, "Магнит")
+        descriptionText.putString(AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE, "Марки Natura, 400 грамм")
+
+        while (!EventRepo.event?.packageName.toString().equals("com.fdrinc.mylist")) {
+            delay(1000)
+            Log.d("Event", EventRepo.event?.packageName.toString())
+        }
+
         val addButton: AccessibilityNodeInfo =
-            root.findAccessibilityNodeInfosByViewId("com.fdrinc.mylist:id/add_button").first()
+            EventRepo.rootElement?.findAccessibilityNodeInfosByViewId("com.fdrinc.mylist:id/add_button")!!.first()
         addButton.performAction(ACTION_CLICK)
-    }
 
-    fun debugClick( event: AccessibilityEvent) {
-        Log.d(
-            "Element info", "ClassName:" + event.className +
-                    " Text:" + event.text +
-                    " ViewIdResourceName:" + event.source.viewIdResourceName
-            // " isClickable:" + event.
-        )
-    }
+        delay(1000)
+        val nameField: AccessibilityNodeInfo = EventRepo.rootElement?.findAccessibilityNodeInfosByViewId("com.fdrinc.mylist:id/name_more_info")!!.first()
+        nameField.performAction(ACTION_SET_TEXT, nameText)
 
-    fun debugView(event: AccessibilityEvent) {
-        Log.d("Parent: ", event.source.parent.toString())
+        delay(1000)
+        val whereToBuyField: AccessibilityNodeInfo = EventRepo.rootElement?.findAccessibilityNodeInfosByViewId("com.fdrinc.mylist:id/where_to_buy_more_info")!!.first()
+        whereToBuyField.performAction(ACTION_SET_TEXT, whereToBuyText)
+
+        delay(1000)
+        val descriptionField: AccessibilityNodeInfo = EventRepo.rootElement?.findAccessibilityNodeInfosByViewId("com.fdrinc.mylist:id/description_more_info")!!.first()
+        descriptionField.performAction(ACTION_SET_TEXT, descriptionText)
+
+        delay(1000)
+        val doneButton: AccessibilityNodeInfo =
+            EventRepo.rootElement?.findAccessibilityNodeInfosByViewId("com.fdrinc.mylist:id/more_info_done")!!.first()
+        doneButton.performAction(ACTION_CLICK)
+
+        delay(1000)
+        val cheeseCard: AccessibilityNodeInfo =
+            EventRepo.rootElement?.findAccessibilityNodeInfosByViewId("com.fdrinc.mylist:id/one_item")!!.get(2)
+        if (cheeseCard.isVisibleToUser) {
+            val mainActivity = MainActivity()
+            mainActivity.firstTestDone()
+
+        }
     }
 }
