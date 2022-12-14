d4270ecbfbfde1b511a59f3dc093922a2825de55
Fair-de-Ragon <fairderagon@yandex.ru>
2022-12-14 17:31:17 +0300

    Added working checkBox and it's logic

--- app/src/main/java/com/fdrinc/qaapp/MainActivity.kt
+++ app/src/main/java/com/fdrinc/qaapp/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
 import androidx.lifecycle.lifecycleScope
-import com.fdrinc.qaapp.AccessibilityService.NodeInfo
+import com.fdrinc.qaapp.AccessibilityService.FirstTest
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -19,28 +19,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        checkTests()
+
         runAccessibilityButton = findViewById(R.id.goToSettings)
         runAccessibilityButton.setOnClickListener {
             val goToSettingsAccessibility = Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS)
             startActivity(goToSettingsAccessibility)
         }
 
-        val nodeInfo = NodeInfo()
+        val firstTest = FirstTest()
 
         runTest = findViewById(R.id.runTest)
         runTest.setOnClickListener {
             val startTestingApp = Intent(packageManager.getLaunchIntentForPackage("com.fdrinc.mylist"))
             startActivity(startTestingApp)
             lifecycleScope.launch {
-                nodeInfo.firstTest()
+                firstTest.firstTest()
             }
         }
     }
 
-    fun firstTestDone() {
-        firstTestCheckBox = findViewById(R.id.firstTestCheckBox)
-        firstTestCheckBox.isChecked
+    override fun onResume() {
+        super.onResume()
+        checkTests()
     }
 
+    fun checkTests() {
+        if (TestsStatus.firstTestisDone) {
+            firstTestCheckBox = findViewById(R.id.firstTestCheckBox)
+            firstTestCheckBox.isChecked = true
+        }
+    }
 
 }
